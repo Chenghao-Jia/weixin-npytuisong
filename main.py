2f5debd67148fed0a575f2e2b7cd872212168d25
Chenghao-Jia <77776356+Chenghao-Jia@users.noreply.github.com>
2023-07-09 09:16:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,8 +180,8 @@ def lizhi():
         return ""
         
 
-#下雨概率和建议
-def tip():
+# 实时温度
+def real():
     if (tianqi_API!="替换掉我"):
         conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
         params = urllib.parse.urlencode({'key':tianqi_API,'city':city})
@@ -190,14 +190,14 @@ def tip():
         res = conn.getresponse()
         data = res.read()
         data = json.loads(data)
-        pop = data["newslist"][0]["pop"]
-        tips = data["newslist"][0]["tips"]
-        return pop,tips
+        real = data["newslist"][0]["real"]
+        # tips = data["newslist"][0]["tips"]
+        return real
     else:
         return "",""
 
 #推送信息
-def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, pop, tips, note_en, note_ch, health_tip, lucky_):
+def send_message(to_user, access_token, city_name, weather, max_temperature, min_temperature, pipi, lizhi, real, note_en, note_ch, health_tip, lucky_):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -271,18 +271,18 @@ def send_message(to_user, access_token, city_name, weather, max_temperature, min
                 "color": get_color()
             },
 
-            "pop": {
-                "value": pop,
-                "color": get_color()
-            },
+            # "pcpn": {
+            #     "value": pcpn,
+            #     "color": get_color()
+            # },
 
             "health": {
                 "value": health_tip,
                 "color": get_color()
             },
 
-            "tips": {
-                "value": tips,
+            "real": {
+                "value": real,
                 "color": get_color()
             }
         }
@@ -351,14 +351,14 @@ if __name__ == "__main__":
     #健康小提示
     health_tip = health()
     #下雨概率和建议
-    pop,tips = tip()
+    real = real()
     #励志名言
     lizhi = lizhi()
     #星座运势
     lucky_ = lucky()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, city, weather, max_temperature, min_temperature, pipi, lizhi,pop,tips, note_en, note_ch, health_tip, lucky_)
+        send_message(user, accessToken, city, weather, max_temperature, min_temperature, pipi, lizhi, real, note_en, note_ch, health_tip, lucky_)
     import time
     time_duration = 3.5
     time.sleep(time_duration)
